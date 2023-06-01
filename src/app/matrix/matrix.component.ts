516a3557d00e6057b39135b213e8a6b749635a75
L4uZP <malaura.zam.pa@gmail.com>
2023-06-01 00:04:46 -0500

    feat: game gui & patterns

--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -7,67 +7,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatrixComponent implements OnInit {
   public grid: boolean[][] = [];
-  public isPaused: boolean = false;
-  private width = 100;
-  private height = 50;
+  public isPaused: boolean;
+  private gridWidth: number = 100;
+  private gridHeight: number = 70;
 
   constructor() {
+    this.isPaused = true;
+    this.initGrid();
   }
 
   ngOnInit() {
-    this.createGrid();
+    this.runGame().then();
   }
 
-  public resetGrid(): void {
-    this.createGrid();
+  public initGrid(): void {
+    for (let row = 0; row < this.gridHeight; row++) {
+      this.grid[row] = [];
+      for (let col = 0; col < this.gridWidth; col++) {
+        this.grid[row][col] = false;
+      }
+    }
   }
 
   public setCell(row: number, col: number): void {
     this.grid[row][col] = true;
   }
 
-  public updateGrid(): void {
-    const newGrid: boolean[][] = [];
-    for (let row = 0; row < this.height; row++) {
-      newGrid[row] = [];
-      for (let col = 0; col < this.width; col++) {
-        const isAlive = this.grid[row][col];
-        const aliveNeighbors = this.countAliveNeighbors(row, col);
-        if (isAlive && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
-          // La célula muere por soledad o sobrepoblación
-          newGrid[row][col] = false;
-        } else if (!isAlive && aliveNeighbors === 3) {
-          // Una célula muerta se convierte en viva por reproducción
-          newGrid[row][col] = true;
-        } else {
-          // Mantener el estado actual de la célula
-          newGrid[row][col] = isAlive;
-        }
-      }
-    }
-    this.grid = newGrid;
-  }
-
-  public async runGame(): Promise<void> {
-    while (true) {
-      if (!this.isPaused) {
-        this.updateGrid();
-        await this.sleep(1000); // Esperar 1 segundo entre iteraciones
-      } else {
-        await this.waitUntilResumed();
-      }
-    }
+  public resumeGame(): void {
+    this.isPaused = false;
   }
 
   public pauseGame(): void {
     this.isPaused = true;
   }
 
-  public resumeGame(): void {
-    this.isPaused = false;
+  public rules(): void {
+    const newGrid: boolean[][] = [];
+    for (let row = 0; row < this.gridHeight; row++) {
+      newGrid[row] = [];
+      for (let col = 0; col < this.gridWidth; col++) {
+        const cellIsAlive = this.grid[row][col];
+        const aliveNeighbors = this.countAliveNeighbors(row, col);
+        if (cellIsAlive && (aliveNeighbors < 2 || aliveNeighbors > 3)) {
+          newGrid[row][col] = false; // Muere por soledad o sobrepoblación
+        } else if (!cellIsAlive && aliveNeighbors === 3) {
+          newGrid[row][col] = true; // Nace por reproducción
+        } else {
+          newGrid[row][col] = cellIsAlive; // Vive
+        }
+      }
+    }
+    this.grid = newGrid;
   }
 
-  private waitUntilResumed(): Promise<void> {
+  public createOscillator(): void {
+    const randomCol = Math.floor(Math.random() * this.gridWidth);
+    const randomRow = Math.floor(Math.random() * this.gridHeight);
+
+    if (randomCol + 2 < this.gridWidth && randomRow + 2 < this.gridHeight) {
+      this.grid[randomRow][randomCol] = true;
+      this.grid[randomRow + 1][randomCol] = true;
+      this.grid[randomRow + 2][randomCol] = true;
+    }
+  }
+
+  public createStaticLife(): void {
+    const randomRow = Math.floor(Math.random() * this.gridHeight);
+    const randomCol = Math.floor(Math.random() * this.gridWidth);
+
+    if (randomRow >= 0 && randomRow < this.gridHeight && randomCol >= 0 && randomCol < this.gridWidth) {
+      this.grid[randomRow][randomCol] = true;
+      this.grid[randomRow][randomCol + 1] = true;
+      this.grid[randomRow + 1][randomCol] = true;
+      this.grid[randomRow + 1][randomCol + 1] = true;
+    }
+  }
+
+  public createSpaceship(): void {
+    const randomRow = Math.floor(Math.random() * (this.gridHeight - 4));
+    const randomCol = Math.floor(Math.random() * (this.gridWidth - 5));
+    if (randomRow >= 0 && randomRow + 2 < this.gridHeight && randomCol >= 0 && randomCol + 3 < this.gridWidth) {
+      this.grid[randomRow][randomCol + 1] = true;
+      this.grid[randomRow][randomCol + 4] = true;
+      this.grid[randomRow + 1][randomCol] = true;
+      this.grid[randomRow + 2][randomCol] = true;
+      this.grid[randomRow + 2][randomCol + 4] = true;
+      this.grid[randomRow + 3][randomCol] = true;
+      this.grid[randomRow + 3][randomCol + 1] = true;
+      this.grid[randomRow + 3][randomCol + 2] = true;
+      this.grid[randomRow + 3][randomCol + 3] = true;
+    }
+  }
+
+  public createMethuselah(): void {
+    const row = Math.floor(Math.random() * (this.gridHeight - 3)); // Generar un número aleatorio para la fila
+    const col = Math.floor(Math.random() * (this.gridWidth - 3)); // Generar un número aleatorio para la columna
+
+    // Verificar si la posición generada está dentro de los límites del grid
+    if (row >= 0 && row + 3 < this.gridHeight && col >= 0 && col + 3 < this.gridWidth) {
+      // Agregar el patrón del Matusalén en la posición aleatoria generada
+      this.grid[row + 1][col + 2] = true;
+      this.grid[row + 2][col] = true;
+      this.grid[row + 2][col + 2] = true;
+      this.grid[row + 2][col + 3] = true;
+      this.grid[row + 3][col + 1] = true;
+    }
+  }
+
+  private async runGame(): Promise<void> {
+    while (true) {
+      if (!this.isPaused) {
+        this.rules();
+        await this.sleep(700);
+      } else {
+        await this.wait();
+      }
+    }
+  }
+
+  private sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  private wait(): Promise<void> {
     return new Promise(resolve => {
       const intervalId = setInterval(() => {
         if (!this.isPaused) {
@@ -76,19 +138,6 @@ export class MatrixComponent implements OnInit {
         }
       }, 100);
     });
-  }
-
-  private sleep(ms: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  private createGrid(): void {
-    for (let row = 0; row < this.height; row++) {
-      this.grid[row] = [];
-      for (let col = 0; col < this.width; col++) {
-        this.grid[row][col] = false;
-      }
-    }
   }
 
   private countAliveNeighbors(row: number, col: number): number {
@@ -100,9 +149,9 @@ export class MatrixComponent implements OnInit {
         const neighborCol = col + j;
         if (
           neighborRow >= 0 &&
-          neighborRow < this.height &&
+          neighborRow < this.gridHeight &&
           neighborCol >= 0 &&
-          neighborCol < this.width &&
+          neighborCol < this.gridWidth &&
           this.grid[neighborRow][neighborCol]
         ) {
           count++;
